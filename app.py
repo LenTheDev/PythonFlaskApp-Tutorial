f51a576e3a74c3f5d1b4320bb8ffaef28c277456
LenTheDev <lhl7@njit.edu>
2021-05-09 01:38:54 -0400

    "Feature: I. Creating Your First Flask Application"

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from logic import square_of_number_plus_nine
+
+# Create Flask's `app` object
+app = Flask(
+    __name__,
+    instance_relative_config=False,
+    template_folder="templates",
+    static_folder="static"
+)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
